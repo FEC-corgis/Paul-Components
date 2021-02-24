0677f50a220d7f09ad8d4eb06e1543e8cb33ad77
Paul Van <paulyv8n@gmail.com>
2021-02-24 13:29:22 -0800

    map rendered

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,27 +1,20 @@
 import React, {useState, useEffect, useRef} from 'react';
 import ReactDOM, {render} from 'react-dom';
 import {Route, BrowserRouter as Router} from 'react-router-dom';
+import GoogleMapReact from 'google-map-react';
 import {getDetes} from '../helper/api';
 import './map.css';
 
 const Map = (props) => {
   let locationSection;
-
-  const mapCanvas = () => {
-    let map = new window.google.maps.Map(document.getElementById('mapObject'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
-    });
-  };
-
   const [locationDetes, setLocationDetes] = useState([]);
+  const [mapPos, setMapPos] = useState({center: {lat: 40.73, lng: -73.93}, zoom: 12});
 
   const updateLocationDetes = () => getDetes(props.match.params.id)
     .then(data => setLocationDetes(data));
 
   useEffect(()=>{
     updateLocationDetes();
-    mapCanvas();
   }, []);
 
 
@@ -32,7 +25,21 @@ const Map = (props) => {
         <div className="flexContainer">
           <div className="contentContainer">
             <div className="headerTitle"><h2>Location</h2></div>
-            <div style={{width: 500, height: 500}} id="mapObject"></div>
+            <div className="mapContainer">
+              <GoogleMapReact 
+                bootstrapURLKeys={{
+                  key: 'AIzaSyDjzD_RyxE02CcfG_ZXLNMro2Ek4OFkgzo', 
+                  language: 'en'
+                }}
+                mapContainerStyle={{
+                  height: '480px',
+                  width: '1128px'
+                }}
+                defaultCenter={mapPos.center}
+                center={mapPos.center}
+                defaultZoom={mapPos.zoom}
+              />
+            </div>
             <div className="cityStateCountry"><h1>{locationDetes[0].propertyCity}, {locationDetes[0].propertyState}, {locationDetes[0].propertyCountry}</h1></div>
             <div className="locationDescription">{locationDetes[0].description}</div>
             <a className="mapBtn" href="">More about the location</a>
